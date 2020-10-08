fe5a4b51b168d999821cb83f98ae6b9a0bcd3ddf
Michel Heiniger <git@heiniger.io>
2020-10-08 12:06:47 +0200

    Removed chart.js

--- pages/locations/[locationId].js
+++ pages/locations/[locationId].js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Title from '../../components/title.component';
 
 const Location = ({ data, curLocation, locationId }) => {
