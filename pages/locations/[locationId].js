5c36f91040ea016c5966886bfb48f4ed38501fbf
Michel Heiniger <git@heiniger.io>
2020-10-08 15:25:05 +0200

    removed useEffect

--- pages/locations/[locationId].js
+++ pages/locations/[locationId].js
@@ -1,10 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Title from '../../components/title.component';
 
 const Location = ({ data, curLocation, locationId }) => {
+  const data2 = [
+    { quarter: 1, earnings: 13000 },
+    { quarter: 2, earnings: 16500 },
+    { quarter: 3, earnings: 14250 },
+    { quarter: 4, earnings: 19000 },
+  ];
   return (
     <div>
       <Title>{curLocation.name}</Title>
+      <div className="max-w-screen-md"></div>
     </div>
   );
 };
